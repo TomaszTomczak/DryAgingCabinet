0cc55cd4f6f620a059a3f143ed7352d66ecede12
TomaszTomczak <poczta@tomasztomczak.pl>
2020-03-31 22:50:37 +0200

    sensor issue2

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -2,5 +2,5 @@ import sys
 import Adafruit_DHT
 
 def getHumidityAndTemperature(pin):
-    h,t = Adafruit_DHT.read_retry(pin, Adafruit_DHT.sensor = Adafruit_DHT.DHT22)
+    humi, temp = Adafruit_DHT.read_retry(pin, Adafruit_DHT.sensor = Adafruit_DHT.DHT22)
     return (h,t)
