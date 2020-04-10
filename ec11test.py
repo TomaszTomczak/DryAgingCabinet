b7cca8c59089656301cc2573d84cb1525daf49f5
TomaszTomczak <poczta@tomasztomczak.pl>
2020-04-10 23:25:09 +0200

    rotate detection

--- ec11test.py
+++ ec11test.py
@@ -1,6 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 
+outcome = [0,-1,1,0,-1,0,0,1,1,0,0,-1,0,-1,1,0]
+last_AB = 0b00
+counter = 0
 if __name__ == '__main__':
 
     GPIO.setwarnings(False)    # Ignore warning for now
@@ -14,7 +17,12 @@ if __name__ == '__main__':
         A = GPIO.input(17)
         B = GPIO.input(27)
         SW = GPIO.input(22)
-        outStr = "A: "+str(A)+" B: "+str(B)+" SW: "+str(SW)
+        current_AB = (A<<1) | B
+        position = (last_AB<<2) | current_AB
+        counter += outcome[position]
+        last_AB = current_AB
+
+        outStr = "A: "+str(A)+" B: "+str(B)+" SW: "+str(SW) + "counter: "+ str(counter)
         print(outStr)
         time.sleep(0.01)
 
