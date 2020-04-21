4ea7327a6807406e5a5aa83b9e111dcc29e5cd37
TomaszTomczak <poczta@tomasztomczak.pl>
2020-04-21 21:08:49 +0200

    pin change on ec11

--- ec11test.py
+++ ec11test.py
@@ -11,16 +11,16 @@ if __name__ == '__main__':
     GPIO.setwarnings(False)    # Ignore warning for now
     GPIO.setmode(GPIO.BCM)   
 
-    GPIO.setup(17,GPIO.IN) #A
-    GPIO.setup(27,GPIO.IN) #B
-    GPIO.setup(22,GPIO.IN) #switch (chyba)
+    GPIO.setup(27,GPIO.IN) #A
+    GPIO.setup(22,GPIO.IN) #B
+    GPIO.setup(10,GPIO.IN) #switch (chyba)
 
 #check every 10 loops if value was changed and step it. It will buffer output and reduce noise
 
     while True:
-        A = GPIO.input(17)
-        B = GPIO.input(27)
-        SW = GPIO.input(22)
+        A = GPIO.input(27)
+        B = GPIO.input(22)
+        SW = GPIO.input(10)
         current_AB = (A<<1) | B
         position = (last_AB<<2) | current_AB
         counter += outcome[position]
