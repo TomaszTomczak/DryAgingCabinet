aa62d0e9b936cb67fae9b5e521415b5912379e4f
TomaszTomczak <poczta@tomasztomczak.pl>
2020-05-03 00:32:12 +0200

    changes in input controller

---
+++ interface/inputDevices/ec11_encoder.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+import time
+from input_controller import InputDevice
+
+class InputEncoderEC11(InputDevice):
+    outcome = [0,1,-1,0,-1,0,0,1,1,0,0,-1,0,-1,1,0]
+    # rot_enc_table[]= {0,1,-1,0,-1,0,0,1,1,0,0,-1,0,-1,1,0};
+    last_AB = 0b00
+    lastCounter = 0
+    counter = 0
+    co = 0
+    realValue=0
+
+    def __init__(self, config):
+        self.inputA = config["position"]["A"]
+        self.inputB = config["position"]["B"]
+        self.inputC = config["position"]["C"]
+        self.id = config["id"]
+        GPIO.setwarnings(False)    # Ignore warning
+        GPIO.setmode(GPIO.BCM)   
+
+        GPIO.setup(self.inputA,GPIO.IN) #A
+        GPIO.setup(self.inputB,GPIO.IN) #B
+        GPIO.setup(self.inputC,GPIO.IN) #switch (chyba)
+
+    def update(self, eventQueue):
+        A = GPIO.input(self.inputA)
+        B = GPIO.input(self.inputB)
+        SW = GPIO.input(self.inputC)
+        current_AB = (A<<1) | B
+        position = (self.last_AB<<2) | current_AB
+        self.counter += self.outcome[position]
+        self.last_AB = current_AB
+        if co%100 == 0:
+            if lastCounter < counter:
+                realValue += 1
+                lastCounter = counter
+            elif lastCounter > counter:
+                realValue -= 1
+                lastCounter = counter
+            co = 0
+        co+=1
--- interface/inputDevices/input_device.py
+++
@@ -1,6 +0,0 @@
-class InputDevice:
-    def __init__(self, device_config):
-        pass
-    def update(self):
-        pass
-    
