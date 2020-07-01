53cb42c9ae8be27babacb4493bfef50098d12634
TomaszTomczak <poczta@tomasztomczak.pl>
2020-07-01 17:33:02 +0200

    i2c lcd interface introduction

--- display/lcd_i2c_display.py
+++ display/lcd_i2c_display.py
@@ -8,7 +8,7 @@ import time
 from time import gmtime, strftime
 from display.lcd_printout import Printout
 from copy import copy
-from display.I2C_LCD_driver import I2C_LCD_driver
+import display.I2C_LCD_driver
 from time import *
 
 # To config file:
