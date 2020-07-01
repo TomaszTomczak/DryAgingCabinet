702f97a96666af333ef901aa0612dad7888016a3
TomaszTomczak <poczta@tomasztomczak.pl>
2020-07-01 17:36:33 +0200

    i2c lcd interface introduction 2

--- app.py
+++ app.py
@@ -102,8 +102,8 @@ if __name__ == '__main__':
 
     lcdcont.configure(data["displays"])
     c.configure(data)
-    #lcdcont.update_printout_data('lcd1',lcd_controller.Printout("test", "test1", "test2", 2))
-    #lcdcont.update_printout_data('lcd1',lcd_controller.Printout("test123", "test55", "test66", 3))
+    lcdcont.update_printout_data('lcd1',lcd_controller.Printout("test", "test1", "test2", 2))
+    lcdcont.update_printout_data('lcd1',lcd_controller.Printout("test123", "test55", "test66", 3))
     #lcdcont.update_printout_data('lcd1',climatePrintout)
     print(lcdcont.get_displays_id())
     x = threading.Thread(target=tfunc)
